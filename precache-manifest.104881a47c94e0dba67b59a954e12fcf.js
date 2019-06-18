81d4bd34338962e05eeea8b17d50ab2bfd72ad71
sai <cyrus.besabella@vervespire.com>
2019-06-19 06:26:53 +0800

    initial commit for dist

---
+++ precache-manifest.104881a47c94e0dba67b59a954e12fcf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cf48180ff16745ce6e4e",
+    "url": "/js/chunk-vendors.b3001dd4.js"
+  },
+  {
+    "revision": "1edb05aecfe57c392651",
+    "url": "/js/chunk-e413e6d4.de43f5c0.js"
+  },
+  {
+    "revision": "a1179509002ab45cd8c3",
+    "url": "/js/chunk-2fc27478.d998adf0.js"
+  },
+  {
+    "revision": "fb22a20a1d79e0646eb2",
+    "url": "/js/app.b0eb023a.js"
+  },
+  {
+    "revision": "182afcbccff15e89656c4f9f583672a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf2117d9ece8451ee765b8649874e236",
+    "url": "/img/Lifeloop-logo.cf2117d9.svg"
+  },
+  {
+    "revision": "1edb05aecfe57c392651",
+    "url": "/css/chunk-e413e6d4.8cb76a80.css"
+  },
+  {
+    "revision": "a1179509002ab45cd8c3",
+    "url": "/css/chunk-2fc27478.ba957ca1.css"
+  },
+  {
+    "revision": "fb22a20a1d79e0646eb2",
+    "url": "/css/app.b0008685.css"
+  }
+];
